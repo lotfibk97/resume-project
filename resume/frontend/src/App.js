b59aa3212c691a11286b66ebb2e2202c94ec9a4d
wassil <work.wassdahi@gmail.com>
2021-02-08 19:58:44 +0100

    add react app

---
+++ resume/frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Resume Generator </h1>
+
+
+    </div>
+  );
+}
+
+export default App;
